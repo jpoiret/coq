196c1c783e96d0b137c19c4874dd00c8c4767911
Josselin Poiret <dev@jpoiret.xyz>
2024-07-03 14:21:55 +0200

    Pass Extra tactics + Export from Prelude.

--- theories/Init/Prelude.v
+++ theories/Init/Prelude.v
@@ -12,6 +12,7 @@ Require Export Coq.Init.Notations.
 Require Export Coq.Init.Types.
 Require Export Coq.Init.Tactics.Ltac.
 Require Export Coq.Init.Tactics.Tauto.
+Require Export Coq.Init.Tactics.Extra.
 Declare ML Module "cc_plugin:coq-core.plugins.cc".
 Declare ML Module "firstorder_core_plugin:coq-core.plugins.firstorder_core".
 Declare ML Module "firstorder_plugin:coq-core.plugins.firstorder".

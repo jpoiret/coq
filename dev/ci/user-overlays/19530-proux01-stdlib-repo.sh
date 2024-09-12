d31f02a529ace83700490f9e3360db7562d76668
Pierre Roux <pierre.roux@onera.fr>
2024-09-12 14:56:28 +0200

    Add overlays

---
+++ dev/ci/user-overlays/19530-proux01-stdlib-repo.sh
@@ -0,0 +1,9 @@
+overlay compcert https://github.com/proux01/CompCert stdlib_repo 19530
+overlay elpi https://github.com/proux01/coq-elpi stdlib_repo 19530
+overlay coq_lsp https://github.com/proux01/coq-lsp stdlib_repo 19530
+overlay equations https://github.com/proux01/Coq-Equations stdlib_repo 19530
+overlay flocq https://github.com/proux01/flocq stdlib_repo 19530
+overlay kami https://github.com/proux01/kami stdlib_repo 19530
+overlay metacoq https://github.com/proux01/metacoq stdlib_repo 19530
+overlay quickchick https://github.com/proux01/QuickChick stdlib_repo 19530
+overlay serapi https://github.com/proux01/coq-serapi stdlib_repo 19530

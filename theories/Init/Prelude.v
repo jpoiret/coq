proux01/roots

Add Bool.Sumbool to prelude

theories/Init/Prelude.v
@@ -22,6 +22,7 @@ Require Export Coq.Init.Wf.
 Require Export Coq.Init.Ltac.
 Require Export Coq.Init.Tactics.
 Require Export Coq.Init.Tauto.
+Require Export Coq.Init.Sumbool.
 (* Some initially available plugins. See also:
    - ltac_plugin (in Ltac)
    - tauto_plugin (in Tauto).

jpoiret/coq

theories/Init/Prelude.v
@@ -22,6 +22,7 @@ Require Export Stdlib.Init.Wf.
 Require Export Stdlib.Init.Ltac.
 Require Export Stdlib.Init.Tactics.
 Require Export Stdlib.Init.Tauto.
+Require Export Stdlib.Init.Sumbool.
 (* Some initially available plugins. See also:
    - ltac_plugin (in Ltac)
    - tauto_plugin (in Tauto).
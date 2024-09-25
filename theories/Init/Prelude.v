1b9a8654dd008a0e6908e67bad1de24e16311034
Josselin Poiret <dev@jpoiret.xyz>
2024-09-25 16:15:00 +0200

    Fix Cctac not being loaded in Prelude.

--- theories/Init/Prelude.v
+++ theories/Init/Prelude.v
@@ -14,6 +14,7 @@ Require Export Stdlib.Init.Types.
 Require Export Stdlib.Init.Tactics.Ltac.
 Require Export Stdlib.Init.Tactics.Tauto.
 Require Export Stdlib.Init.Tactics.Extra.
+Declare ML Module "cc_core_plugin:coq-core.plugins.cc_core".
 Declare ML Module "cc_plugin:coq-core.plugins.cc".
 Declare ML Module "firstorder_core_plugin:coq-core.plugins.firstorder_core".
 Declare ML Module "firstorder_plugin:coq-core.plugins.firstorder".

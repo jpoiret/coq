1af4e7caebed38e931c9d68ed687e6b096205193
Tomas <tomdtron@gmail.com>
2025-07-09 14:26:08 +0200

    fix: adjust to_type in minimize_univ after rebase

--- vernac/comFixpoint.ml
+++ vernac/comFixpoint.ml
@@ -464,7 +464,7 @@ let interp_mutual_definition env ~program_mode ~poly ~function_mode rec_order fi
   (* Instantiate evars and check all are resolved *)
   let sigma = Evarconv.solve_unif_constraints_with_heuristics env sigma in
   let sigma = UnivVariances.register_universe_variances_of_fix env sigma fixtypes fixdefs in
-  let sigma = Evd.minimize_universes ~partial:(List.exists Option.is_empty fixdefs) sigma in
+  let sigma = Evd.minimize_universes ~to_type:(not poly) ~partial:(List.exists Option.is_empty fixdefs) sigma in
 
   (* Build the fix declaration block *)
   let fix = {fixnames=fixlnames;fixrs;fixdefs;fixtypes;fixctxs;fiximps;fixntns;fixwfs} in
